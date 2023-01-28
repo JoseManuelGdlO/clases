da297051730d88b48fc149028ccd277c5a494ca0
Jose Manuel <josemanuelgarciadela@gmail.com>
2023-01-28 10:15:46 -0600

    hola  mundo

---
+++ Assets/acciones.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class acciones : MonoBehaviour
+{
+	public TextMeshProUGUI output;
+	public TextMeshProUGUI input; 
+
+	public void MiFuncion() {
+		string miTexto = input.text;
+		output.SetText(miTexto);
+	}
+}
---
+++ Assets/functions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class functions : MonoBehaviour
+{
+	public Button Do, Re, Mi, Fa, Sol,La, Si, Doo, Ree, Faa, Soll, Laa;
+	
+	public Button buttonIWantToUse;
+	
+	
+ 
+    // Start is called before the first frame update
+    void Start()
+	{
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			Do.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.W)) {
+			Re.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.E)) {
+			Mi.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.R)) {
+			Fa.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.T)) {
+			Sol.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.Y)) {
+			La.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.U)) {
+			Si.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.Alpha2)) {
+			Doo.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.Alpha3)) {
+			Ree.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.Alpha4)) {
+			Faa.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.Alpha5)) {
+			Soll.onClick.Invoke();
+		} else if(Input.GetKeyDown(KeyCode.Alpha6)) {
+			Laa.onClick.Invoke();
+		}
+	}
+}
